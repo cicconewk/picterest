9baf2e57bf9b7fdfccbec1619e64644cfd7a988c
cicconewk <cicconewk@gmail.com>
2019-04-16 21:34:23 -0300

    updating base files

---
+++ src/database.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(URI, {useNewUrlParser: true, useCreateIndex: true})
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const multer = require('multer')
+const path = require('path')
+
+app.set('PORT', process.env.PORT || 3000)
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(multer({dest: path.join(__dirname,'public','img','uploads')}).single('image'))
+
+app.use(require(path.join(__dirname,'routes','index.routes.js')))
+
+app.listen(3000, () => {
+  console.log('Express server running on PORT 3000')
+})
