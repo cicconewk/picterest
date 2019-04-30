01451eacc3f46e8de66df5cdf1b7f87c9b2d2c32
cicconewk <cicconewk@gmail.com>
2019-04-30 00:31:14 -0400

    Database connection done. Fixed routes. **ADD: Implement controllers for each route.

--- src/server/database.js
+++ src/server/database.js
@@ -1,3 +1,10 @@
 const mongoose = require('mongoose')
+const environment = process.env.NODE_ENV || 'development'
+const {user, password, host, port, dialect, database} = require('./config/dbconfig.json')[environment]
 
-mongoose.connect(URI, {useNewUrlParser: true, useCreateIndex: true})
+mongoose.connect(`${dialect}://${user}:${password}@${host}:${port}/${database}`, 
+  {useNewUrlParser: true, useCreateIndex: true}, (err) => {
+    if (err) throw new Error(err)
+
+    console.log(`Database ${database} connected successfully.`)
+  })
--- src/server/index.js
+++ src/server/index.js
@@ -3,15 +3,16 @@ const app = express()
 const morgan = require('morgan')
 const multer = require('multer')
 const path = require('path')
+require('./database')
 
 app.set('PORT', process.env.PORT || 3000)
 
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 
-app.use('api/users', require(path.join(__dirname,'routes','users.routes')))
-app.use('api/pictures', require(path.join(__dirname,'routes','pictures.routes')))
+app.use('/api/users', require(path.join(__dirname,'routes','users.routes')))
+app.use('/api/pictures', require(path.join(__dirname,'routes','pictures.routes')))
 app.use((req, res, next) => {
   return res.status(404).json({"message": "Resource not found."})
 })
